2277e166a7696f48034c6a56563bd448c0c0cca2
realcow <realcow86@gmail.com>
2014-07-28 10:53:49 +0900

    한자키 작동이 다른 키와 다르다. IsHanjaEnabled위에 있는 코멘트 참고

--- KeyboardHookAgent/GlobalKeyHook/GlobalKeyHook.cpp
+++ KeyboardHookAgent/GlobalKeyHook/GlobalKeyHook.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <Utils/WindowActivator/ProcessActivator.h>
+#include <crtdbg.h>
 
 using namespace std;
 
@@ -39,17 +40,49 @@ vector<KeyHandlerEntry> keyHandlers =
     { VK_DOWN, OnArrowKey },
     { VK_RIGHT, OnArrowKey },
 
-    { VK_HOME, bind([](WORD key, const wstring& s){ ActivateProcess(s); }, std::placeholders::_1, L"WDExpress.exe") },
+    { VK_HOME, bind([](WORD key, const wstring& s){ ActivateProcess(s); }, std::placeholders::_1, L"devenv.exe") },
+    //{ VK_HOME, bind([](WORD key, const wstring& s){ ActivateProcess(s); }, std::placeholders::_1, L"WDExpress.exe") },
     { VK_END, bind([](WORD key, const wstring& s){ ActivateProcess(s); }, std::placeholders::_1, L"notepad++.exe") },
 };
 
+// *NOTE(realcow):
+// VK_HANJA 후킹시 keydown은 발생하는데 keyup이 발생하지 않음. (WH_KEYBOARD, WH_KEYBOARD_LL)
+// IME와 관련이 있는 것 같은데 이것 때문에 현재 눌려진 키 조합이 한자키를 포함하고 있는지 알 수 없는 상태.
+// 문제 해결을 위해 취해야할 액션
+//  - IME 동작을 조사해서 해결법을 찾아본다
+//  - DirectInput을 써본다?
+//  - 이것도 저것도 다 안되면 키보드 필터 드라이버를 쓴다.
+// 그래서 현재는 마지막 한자키가 눌린 시간으로부터 일정시간 동안 한자키가 눌렸다고 간주하는 방식으로 하고 있음. 이 방식으로는 home, end 단축키만 대강 작동하는 상태 ㅠ
+bool IsHanjaEnabled(WPARAM wParam, LPARAM lParam)
+{
+    static DWORD64 hanjaKeyLastPressedTime = 0;
+    DWORD64 now = ::GetTickCount64();
+
+    if (wParam == VK_HANJA)
+    {
+        _RPT0(_CRT_WARN, "hanja");
+        hanjaKeyLastPressedTime = now;
+        return true;
+    }
+    else
+    {
+        DWORD64 kHanjaKeyLastingTime = 100;
+        if (now - hanjaKeyLastPressedTime < kHanjaKeyLastingTime)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 LRESULT CALLBACK KeyHookProc(int code, WPARAM wParam, LPARAM lParam)
 {
+    static bool hanjaPressed = false;
     do
     {
         if (code != HC_ACTION) { break; }
-
-        if ((::GetKeyState(VK_HANJA) & 0x8000) == 0) { break; }
+        
+        if (!IsHanjaEnabled(wParam, lParam)) { break; }
 
         auto it = find_if(keyHandlers.cbegin(), keyHandlers.cend(), [&](const KeyHandlerEntry& entry) {
             return entry.key == wParam;
